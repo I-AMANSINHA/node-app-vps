4f337cfb0be27105ed80958c277dfefff3e2ad58
aman-sinha <amansinha4698@gmail.com>
2026-06-19 16:46:10 +0530

    added file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello from Jenkins CI/CD',
+        timestamp: new Date()
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Running on ${PORT}`);
+});
