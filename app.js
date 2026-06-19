6676ae2984a41025657b40fa769facb174cbcc20
aman-sinha <amansinha4698@gmail.com>
2026-06-19 16:48:14 +0530

    modified

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
     res.send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Running on ${PORT}`);
